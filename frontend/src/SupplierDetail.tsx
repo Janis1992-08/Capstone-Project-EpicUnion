620260cce73491c50baf7ef601bb6384636d0985
Janis Schiller <schillerjanis@gmail.com>
2024-08-12 20:16:29 +0200

    add CRUD suppliers Backend

--- frontend/src/SupplierDetail.tsx
+++ frontend/src/SupplierDetail.tsx
@@ -1,16 +1,18 @@
-import {useState, useEffect, useCallback} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import { useParams, useNavigate, Link } from 'react-router-dom';
-import { Supplier} from "./components/FrontendSchema.ts";
+import {Supplier, Task} from "./components/FrontendSchema.ts";
 import Modal from "./components/Modal.tsx";
 import ConfirmModal from "./components/ConfirmModal.tsx";
 import {deleteSupplier, getSupplierById} from "./api/SupplierService.ts";
 import UpdateSuppliersForm from "./components/supplierComponents/UpdateSuppliersForm.tsx";
 import "./styling/SupplierDetail.css";
+import {getTasks} from "./api/TaskService.ts";
 
 
 export default function SupplierDetail() {
     const { id } = useParams<{ id: string }>();
     const [supplier, setSupplier] = useState<Supplier | null>(null);
+    const [tasks, setTasks] = useState<Task[]>([]);
     const navigate = useNavigate();
     const [isVisible, setIsVisible] = useState(false);
     const [isConfirmVisible, setIsConfirmVisible] = useState(false);
@@ -21,10 +23,17 @@ export default function SupplierDetail() {
         }
     }, [id]);
 
+    const fetchTasks = useCallback(() => {
+        getTasks()
+            .then(response => setTasks(response.data))
+            .catch(error => console.error(error));
+    }, []);
+
 
     useEffect(() => {
         fetchSupplier();
-    }, [fetchSupplier]);
+        fetchTasks();
+    }, [fetchSupplier, fetchTasks]);
 
     if (!supplier) {
         return <p>Loading...</p>;
@@ -54,6 +63,28 @@ export default function SupplierDetail() {
         closeModal();
     };
 
+    const getTaskNames = (tasksIds: string[]): React.ReactElement => {
+        if (tasksIds.length === 0) return <span>Unassigned</span>;
+
+        return (
+            <>
+                {tasksIds.map((tasksId, index) => {
+                    const task = tasks.find(t => t.id === tasksId);
+                    if (!task) return null;
+
+                    return (
+                        <span key={tasksId}>
+                                <Link to={`/tasks/${tasksId}`} className="supplier-detail__task-link">
+                                    {task.title}
+                                </Link>
+                            {index < tasksIds.length - 1 && ', '}
+                            </span>
+                    );
+                })}
+            </>
+        );
+    }
+
     return (
         <>
             <div className="supplier-detail">
@@ -62,19 +93,22 @@ export default function SupplierDetail() {
                 </div>
                 <ul className="supplier-detail__list">
                     <li className="supplier-detail__list-item" key={supplier.id}>
-                        <h2 className="supplier-detail__supplier-title" >Name: {supplier.name}</h2>
-                        <p className="supplier-detail__supplier-description" >Description: {supplier.description}</p>
-                        <p className="supplier-detail__supplier-info" >Website: {supplier.websiteUrl}</p>
-                        <p className="supplier-detail__supplier-info" >Costs: {supplier.costs}</p>
-                        <p className="supplier-detail__supplier-info" >Delivery Date: {supplier.deliveryDate}</p>
-                        <p className="supplier-detail__supplier-info" >Phone: {supplier.contactPhone}</p>
-                        <p className="supplier-detail__supplier-info" >Email {supplier.contactEmail}</p>
-                        <p className="supplier-detail__supplier-info" >Address {supplier.contactAddress}</p>
-                            <button className="supplier-detail__button" onClick={openModal}>Update Supplier</button>
-                            <Modal isVisible={isVisible} onClose={closeModal}>
-                                <UpdateSuppliersForm initialSupplier={supplier} onSave={handleSupplierUpdate}/>
-                            </Modal>
-                            <button className="supplier-detail__button supplier-detail__button--delete" onClick={handleDelete}>Delete Supplier</button>
+                        <h2 className="supplier-detail__supplier-title">Name: {supplier.name}</h2>
+                        <p className="supplier-detail__supplier-description">Description: {supplier.description}</p>
+                        <p className="supplier-detail__supplier-info">Website: {supplier.websiteUrl}</p>
+                        <p className="supplier-detail__supplier-info">Costs: {supplier.costs}</p>
+                        <p className="supplier-detail__supplier-info">Delivery Date: {supplier.deliveryDate}</p>
+                        <p className="supplier-detail__supplier-info">Phone: {supplier.contactPhone}</p>
+                        <p className="supplier-detail__supplier-info">Email {supplier.contactEmail}</p>
+                        <p className="supplier-detail__supplier-info">Address {supplier.contactAddress}</p>
+                        <p className="supplier-detail__supplier-info">Tasks: {getTaskNames(supplier.assignedTasks)}</p>
+                        <button className="supplier-detail__button" onClick={openModal}>Update Supplier</button>
+                        <Modal isVisible={isVisible} onClose={closeModal}>
+                            <UpdateSuppliersForm initialSupplier={supplier} onSave={handleSupplierUpdate}/>
+                        </Modal>
+                        <button className="supplier-detail__button supplier-detail__button--delete"
+                                onClick={handleDelete}>Delete Supplier
+                        </button>
                     </li>
                 </ul>
             </div>
--- frontend/src/SupplierList.tsx
+++ frontend/src/SupplierList.tsx
@@ -1,23 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import { Supplier} from "./components/FrontendSchema.ts";
+import React, { useEffect, useState} from 'react';
+import {Supplier, Task} from "./components/FrontendSchema.ts";
 import { Link } from 'react-router-dom';
 import Modal from "./components/Modal.tsx";
 import {getSuppliers} from "./api/SupplierService.ts";
 import AddSupplierForm from "./components/supplierComponents/AddSupplierForm.tsx";
 import "./styling/SupplierList.css"
+import {getTasks} from "./api/TaskService.ts";
 
 export default function SupplierList() {
     const [suppliers, setSuppliers] = useState<Supplier[]>([]);
+    const [tasks, setTasks] = useState<Task[]>([]);
     const [isVisible, setIsVisible] = useState(false);
     const [filter, setFilter] = useState<string>('');
 
+
     const fetchSuppliers = () => {
-        getSuppliers().then(response => setSuppliers(response.data))
+        getSuppliers()
+            .then(response => setSuppliers(response.data))
             .catch(error => console.error('Error fetching suppliers:', error));
+    };
+
+    const fetchTasks = () => {
+        getTasks()
+            .then(response => setTasks(response.data))
+            .catch(error => console.error(error));
     };
 
     useEffect(() => {
         fetchSuppliers();
+        fetchTasks();
     }, []);
 
     const closeModal = () => setIsVisible(false);
@@ -32,12 +43,22 @@ export default function SupplierList() {
         setFilter(event.target.value.toLowerCase());
     };
 
+    const getTaskNames = (tasksIds: string[]) => {
+        return tasksIds.map(tasksId => {
+            const task = tasks.find(t => t.id === tasksId);
+            return task ? task.title : 'Unassigned';
+        }).join(', ');
+    };
+
+
+
     const filteredSuppliers = suppliers.filter(supplier => {
         return supplier.name.toLowerCase().includes(filter) ||
             supplier.description.toLowerCase().includes(filter) ||
             supplier.contactEmail.toLowerCase().includes(filter) ||
             supplier.contactPhone.toLowerCase().includes(filter) ||
-            supplier.contactAddress.toLowerCase().includes(filter);
+            supplier.contactAddress.toLowerCase().includes(filter) ||
+            getTaskNames(supplier.assignedTasks).toLowerCase().includes(filter.toLowerCase())
     });
 
     return (
@@ -66,6 +87,7 @@ export default function SupplierList() {
                             <p className="supplier-list__supplier-info">Costs: {supplier.costs}</p>
                             <p className="supplier-list__supplier-info">Delivery Date: {supplier.deliveryDate}</p>
                             <p className="supplier-list__supplier-info">Email: {supplier.contactEmail}</p>
+                            <p className="supplier-list__supplier-info">Tasks: {getTaskNames(supplier.assignedTasks)}</p>
                         </Link>
                     </li>
                 ))}
